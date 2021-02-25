424891e00934931c9b40bd9dcc4554afc6e40026
leonardosibela <leonardo_sibela@hotmail.com>
2021-02-25 17:26:18 -0300

    Implementing model

--- app/src/main/java/com/sibela/storageroom/Task.kt
+++ app/src/main/java/com/sibela/storageroom/Task.kt
@@ -1,8 +1,16 @@
 package com.sibela.storageroom
 
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "taks")
 class Task(
 
+    @PrimaryKey(autoGenerate = true)
+    @ColumnInfo(name = "id")
     var id: Int = 0,
 
+    @ColumnInfo(name = "title")
     var name: String
 )
